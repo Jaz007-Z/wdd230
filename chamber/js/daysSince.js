fe274b18fddaa999759b5265bcd0a752eb0b90ee
Jaz007-Z <65184285+Jaz007-Z@users.noreply.github.com>
2022-02-17 16:24:04 -0500

    done aside from a few typos

---
+++ chamber/js/daysSince.js
@@ -0,0 +1,22 @@
+const lastVisit = localStorage.getItem('lastVisit');
+
+const FACTOR = 1000 * 60 * 60 * 24;
+
+let daysBetween = Date.now() - lastVisit;
+
+let numberOfDays = Math.round(daysBetween / FACTOR);
+// let numberOfDays = daysBetween;
+
+console.log(numberOfDays);
+
+const daysSince = document.querySelector('#daysSince');
+
+if (lastVisit == null) {
+    daysSince.innerHTML = `Welcome to your first Visit!`
+} else {
+    daysSince.innerHTML = `Days since last visit: ${numberOfDays}`
+}
+
+localStorage.setItem('lastVisit', Date.now());
+
+
