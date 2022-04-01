c8d3962e8dd258d07403f25ce193437315247e5a
Jaz007-Z <65184285+Jaz007-Z@users.noreply.github.com>
2022-04-01 16:40:04 -0400

    updated weather and images

---
+++ templeProject/js/temples.js
@@ -0,0 +1,201 @@
+
+const requestURL = 'json/temples.json';
+// const requestURL = 'https://jaz007-z.github.io/wdd230/Temple/json/temples.json';
+
+const cards = document.querySelector('.cards');
+
+
+async function getTemples() {
+  const apiURL = 'json/temples.json';
+  const response = await fetch(apiURL);
+  const jsonObject = await response.json();
+  const temples = jsonObject['temples'];
+  console.log(temples);
+  await temples.forEach(displayTemples);
+  // displayTemples(temples[0]);
+  addCardEvent();
+}
+
+function displayTemples(temple, x = 0) {
+  console.log(`displayTemples i: ${x}`)
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  // const card = document.querySelector('.card');
+  // console.log(temple);
+
+
+  let image = document.createElement('img');
+  let imageDiv = document.createElement('div');
+
+
+  let infoDiv = document.createElement('div');
+  let infoDivBasics = document.createElement('div');
+  let infoDivExtra = document.createElement('div');
+  let website = document.createElement('a');
+  let name = document.createElement('h3');
+  let phase = document.createElement('p');
+  let address = document.createElement('p');
+  let telephone = document.createElement('p');
+  let servicesDiv = document.createElement('div');
+  let services = document.createElement('p');
+  let scheduleInfo = document.createElement('p');
+  let ordinanceSchedule = document.createElement('p');
+  let sessionSchedule = document.createElement('p');
+
+
+
+
+  // Change the textContent property of the h2 element to contain the temple's full name
+  website.textContent = temple.website;
+  name.textContent = temple.name;
+  phase.textContent = temple.phase;
+  services.textContent = "Services: ";
+  telephone.textContent = `PhoneNumber: ${temple.phoneNumber}`;
+  address.textContent = temple.address;
+
+  scheduleInfo.textContent = "Schedule: ";
+  ordinanceSchedule.textContent = temple.ordinanceSchedule;
+  sessionSchedule.textContent = temple.sessionSchedule;
+  // button.textContent = "Make a Reservation!";
+
+  // servicesPrep = temple.services.filter(temple => (temple.services != "No"));
+  servicesPrep = temple.services;
+  servicesFiltered = servicesPrep.filter(service => (service[0] != 'N' && service[1] != 'o'));
+  // console.log(servicesFiltered);
+
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  card.setAttribute('class', "section card");
+  image.setAttribute('src', temple.imageurl);
+  image.setAttribute('alt', temple.name);
+  image.setAttribute("class", "cardImage")
+  image.setAttribute('loading', 'lazy');
+  imageDiv.setAttribute('class', 'imageCardDiv')
+  infoDiv.setAttribute('class', "templeInfoCard");
+  // infoDiv.setAttribute('class', "templeInfo templeInfoCard");
+  servicesDiv.setAttribute('class', "box templeServicesCard");
+  infoDivBasics.setAttribute('class', "box templeInfoBasicsCard");
+  infoDivExtra.setAttribute('class', `box extraTempleInfo extraTempleInfo${x}`)
+
+
+
+  // Add/append the section(card) with the h2 element
+  // card.appendChild(name);
+  cards.appendChild(card);
+  // card.appendChild(image); put in infodiv
+  card.appendChild(infoDiv);
+  // infoDiv.appendChild(image); //put image in infoDiv
+  infoDiv.appendChild(imageDiv);
+  imageDiv.appendChild(image);
+  infoDiv.appendChild(infoDivBasics);
+  infoDivBasics.appendChild(name);
+  infoDivBasics.appendChild(phase);
+  infoDivBasics.appendChild(address);
+  infoDivBasics.appendChild(telephone);
+  infoDiv.appendChild(servicesDiv);
+  servicesDiv.appendChild(services);
+  for (i = 0; i < temple.services.length; i++) {
+    let service = document.createElement('p');
+    service.textContent = temple.services[i];
+    servicesDiv.appendChild(service);
+  }
+
+
+  infoDiv.appendChild(infoDivExtra);
+  infoDivExtra.appendChild(scheduleInfo);
+  infoDivExtra.appendChild(ordinanceSchedule);
+  infoDivExtra.appendChild(sessionSchedule);
+
+  let historyIntro = document.createElement('p');
+  historyIntro.textContent = "History: ";
+  infoDivExtra.appendChild(historyIntro);
+  for (i = 0; i < temple.history.length; i++) {
+    let history = document.createElement('p');
+    history.textContent = temple.history[i];
+    infoDivExtra.appendChild(history);
+  }
+  let templeClosureScheduleIntro = document.createElement('p');
+  templeClosureScheduleIntro.textContent = "Temple Closure Schedule: ";
+  infoDivExtra.appendChild(templeClosureScheduleIntro);
+  for (i = 0; i < temple.templeClosureSchedule.length; i++) {
+    let templeClosureSchedule = document.createElement('p');
+    templeClosureSchedule.textContent = temple.templeClosureSchedule[i];
+    infoDivExtra.appendChild(templeClosureSchedule);
+
+  }
+  addLikeButton(imageDiv, x);
+
+}
+
+function addLikeButton(infoDiv, x) {
+  console.log(`addKLikeButton i: ${x}`) 
+  let heart = document.createElement('img');
+  heart.setAttribute('alt', "Heart");
+  heart.setAttribute('src', "images/heart.png");
+  heart.setAttribute('class', 'heart');
+  if (window.localStorage.getItem(`favorite${x}`)) {
+    heart.classList.toggle('favorite');
+  }
+  heart.addEventListener('click', () => {
+    console.log("heart touched");
+    if (window.localStorage.getItem(`favorite${x}`)) {
+      window.localStorage.removeItem(`favorite${x}`);
+      heart.classList.toggle('favorite');
+    } else {
+      window.localStorage.setItem(`favorite${x}`, 'on');
+      heart.classList.toggle('favorite');
+    }
+    event.stopImmediatePropagation();
+  }, false);
+
+
+  infoDiv.appendChild(heart);
+}
+
+
+function addCardEvent() {
+  const card = document.querySelectorAll('.card');
+
+  // const infoDiv = document.querySelectorAll('.templeInfo');
+  const infoDiv = document.querySelectorAll('.extraTempleInfo');
+
+  // console.log(mainnav);
+
+
+  for (var i = 0; i < card.length; i++) {
+    const infoDiv = document.querySelector(`.extraTempleInfo${i}`);
+    card[i].addEventListener('click', () => { console.log("card clickedF"); }, false);
+    card[i].addEventListener('click', () => {infoDiv.classList.toggle('extraTempleInfo')}, false);
+
+    infoDivTest = infoDiv[i];
+    console.log(infoDivTest);
+    // card[i].addEventListener('click', () => { infoDivTest.classList.toggle('showExtraInfo') }, false);
+    // card[i].addEventListener('click', () => {mainnav.classList.toggle('showExtraInfo')}, false);
+    // card[i].addEventListener('click', () => { infoDiv[i].classList.toggle('showExtraInfo') }, false);
+    // card[i].addEventListener('click', () => { infoDiv.classList.toggle('showExtraInfo') }, false);
+
+
+    // card[i].addEventListener('click', () => {infoDiv[i].classList.add('.showExtraInfos')}, once="true");
+
+  }
+}
+getTemples();
+
+// const card = document.querySelectorAll('.card');
+
+const headerTop = document.querySelector('.headerTop');
+
+
+// console.log(cards);
+// const infoDiv = document.querySelectorAll('.extraTempleInfo');
+
+// // card.addEventListener('click', () => {infoDiv.classList.toggle('extraTempleInfo')}, false);
+// // if (card) {
+// //   card.addEventListener('click', () => { console.log("card clickedF"); }, false);
+// //   menuButton.addEventListener('click', () => { console.log("card clickedF"); }, false);
+// // }
+// menuButton.addEventListener('click', () => {console.log("card clickedF");}, false);
+// for(var i = 0; i < card.length;i++) {
+// card[i].addEventListener('click', () => {console.log("card clickedF"); }, false);
+// }
+// // card.addEventListener('click', () => {console.log("card clickedF"); }, false);
---
+++ templeProject/js/weather.js
@@ -0,0 +1,87 @@
+
+async function getWheather() {
+    // [38.9807, -77.1003]
+    const exclude = "minutely,current"
+    const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=38.9807&lon=-77.1003&units=imperial&appid=af98801390ad8c67d0b28ab0ca82c8cb`;
+    const response = await fetch(apiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    document.querySelector('#temp').innerHTML = jsObject.current.temp;
+    document.querySelector('#speedWind').innerHTML = jsObject.current.wind_speed;
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.current.weather[0].icon}.png`;
+    let desc = jsObject.current.weather[0].description;
+    desc = desc.split(" ").map(capitalize).join(' ');
+    document.querySelector('#weatherIcon').setAttribute('src', iconsrc);
+    document.querySelector('#weatherIcon').setAttribute('alt', desc);
+    document.querySelector('#weatherDesc').textContent = desc;
+    document.querySelector("#humidity").innerHTML = jsObject.current.humidity;
+
+    // console.log(`Daily tomorrow attempt: ${daily}`)
+    document.querySelector('#temp1').innerHTML = jsObject.daily[1].temp.day;
+    const iconsrc1 = `https://openweathermap.org/img/w/${jsObject.daily[1].weather[0].icon}.png`;
+    let desc1 = jsObject.daily[1].weather[0].description;
+
+    document.querySelector('#temp2').innerHTML = jsObject.daily[2].temp.day;
+    const iconsrc2 = `https://openweathermap.org/img/w/${jsObject.daily[2].weather[0].icon}.png`;
+    let desc2 = jsObject.daily[2].weather[0].description;
+
+    document.querySelector('#temp3').innerHTML = jsObject.daily[3].temp.day;
+    const iconsrc3 = `https://openweathermap.org/img/w/${jsObject.daily[3].weather[0].icon}.png`;
+    let desc3 = jsObject.daily[3].weather[0].description;
+
+    // for future weather, using current weather as placeholder icon and desc
+    document.querySelector('#weatherIcon1').setAttribute('src', iconsrc1);
+    document.querySelector('#weatherIcon1').setAttribute('alt', desc1);
+    document.querySelector('#weatherDesc1').textContent = desc1;
+
+    document.querySelector('#weatherIcon2').setAttribute('src', iconsrc2);
+    document.querySelector('#weatherIcon2').setAttribute('alt', desc2);
+    document.querySelector('#weatherDesc2').textContent = desc2;
+
+    document.querySelector('#weatherIcon3').setAttribute('src', iconsrc3);
+    document.querySelector('#weatherIcon3').setAttribute('alt', desc3);
+    document.querySelector('#weatherDesc3').textContent = desc3;
+
+    if (jsObject.alerts.event) {
+        document.querySelector(".banner").style.display = "block";
+
+        document.querySelector(".banner__close").addEventListener("click", function () {
+            this.closest(".banner").style.display = "none";
+        });
+        document.querySelector("alert").textContent = jsObject.alerts.event;
+        document.querySelector("alertDesc").textContent = jsObject.alerts.description;
+    }
+
+    
+
+
+
+
+
+    // let t = jsObject.main.temp;
+    // let s = jsObject.wind.speed;
+    // let windchill = "";
+
+    // if (t <= 50 && s > 3) {
+    //   windchill = windChill(t, s);
+    //   windchill = `${windchill}&#176;F`;
+    // } else {
+    //   windchill = "N/A";
+    // }
+    // output
+
+
+
+}
+
+//   function windChill(temp, speed) {
+//     let windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+//     return windChill.toFixed(2);
+//   }
+
+function capitalize(word) {
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+}
+
+// run main function
+getWheather();
